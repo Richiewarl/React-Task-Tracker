a06195fa9ada1cd626e51b388c09eddf83d78b69
Richiewarl <leechyesoon@gmail.com>
2022-10-28 23:08:43 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header title="Hello World!" />
+    </div>
+  );
+}
+
+export default App;
