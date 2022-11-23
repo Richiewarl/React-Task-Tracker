7df0ce30c98fc1a1e82cf0e5b75b89195e328665
Richiewarl <leechyesoon@gmail.com>
2022-11-23 21:19:22 +0000

    Implement AddTask logic

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,47 @@
-const AddTask = () => {
+import { useState } from "react"
+
+const AddTask = ({ onAdd }) => {
+  const [text, setText] = useState()
+  const [day, setDay] = useState()
+  const [reminder, setReminder] = useState(false)
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (!text) { alert("Please add a task") }
+
+    onAdd({ text, day, reminder })
+
+    setText("")
+    setDay("")
+    setReminder(false)
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSubmit}>
       <div className="form-control">
           <label>New Task</label>
-          <input type="text" placeholder="New Task..." />
+          <input
+            type="text" 
+            value={text} 
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Add Task..." />
       </div>
       <div className="form-control">
           <label>Day & Time</label>
-          <input type="text" placeholder="New Task..." />
+          <input 
+            type="text" 
+            value={day} 
+            onChange={(e) => setDay(e.target.value)}  
+            placeholder="Add Day and Time..." />
       </div>
       <div className="form-control form-control-check">
           <label>Set Reminder</label>
-          <input type="checkbox" />
+          <input 
+            type="checkbox"
+            // checked={reminder}
+            onChange={(e) => setReminder(e.currentTarget.checked)}  
+            value={reminder} />
       </div>
 
       <input className="btn btn-block" type="submit" value="Save Task" />
